47aded4632102d42e8252f540d68512649e61e2e
Evandro Zanatta <hemanstifler@gmail.com>
2015-04-17 03:29:28 +0000

    Criando o hello world

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/medoo/medoo.min.php';
+require 'vendor/autoload.php';
+
+
+$app = new \Slim\Slim(array(
+    'debug' => true
+));
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
+?>
