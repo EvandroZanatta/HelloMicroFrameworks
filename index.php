f7e5a66cae5a46308479d16a9d3a7b5f9e1fbf70
Evandro Zanatta <hemanstifler@gmail.com>
2015-04-17 03:49:15 +0000

    Adicionando validação dos dados

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Content-Type: text/html; charset=utf-8');
+
 require 'vendor/medoo/medoo.min.php';
 require 'vendor/autoload.php';
 
@@ -8,8 +10,32 @@ $app = new \Slim\Slim(array(
     'debug' => true
 ));
 
+$gump = new GUMP();
+
+function validaEntrada($name){
+    $data = array(
+        'nome' => $name
+    );
+    
+    $validated = GUMP::is_valid($data, array(
+        'nome' => 'required|valid_name'
+    ));
+    
+    if($validated === true) {
+        return true;
+    } else {
+        return false;
+    }    
+}
+
+
 $app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
+    if(validaEntrada($name)){
+        echo "Hello, " . utf8_decode($name);    
+    }else{
+        echo utf8_decode("Você deve ser um humano para usar este site");
+    }
+    
 });
 
 $app->run();
